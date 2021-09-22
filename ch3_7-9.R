cd9aa2afb02cb57625531b0c4a37d03792097c8b
Youheng Lü <youheng.lue@gmail.com>
2021-09-22 13:46:58 +0200

    Finish ch3_7-9 problem 103

--- ch3_7-9.R
+++ ch3_7-9.R
@@ -22,21 +22,48 @@ rho_x = 5
 rho_y = 10
 rho <- c(rho_x,rho_y) # mean
 # covariance matrix:
-# TODO Not Cov but Correlation Coefficient
-# Var[Y], Correlation Coefficient
-# Correlation Coefficient[X,Y], Var[Y]
+# Var[Y], Cov[X,Y]
+# Cov[X,Y], Var[Y]
 var_x = 0.01
 var_y = 0.04
-coeff_xy = 0.8
-c2 <- matrix(c(var_x,coeff_xy,coeff_xy,var_y), nrow = 2, byrow = F) # cov matrix
+# COV[X,Y] = corr_coeff * sqrt(var_x,var_y)
+cov_xy = 0.8*sqrt(var_y*var_x)
+c2 <- matrix(c(var_x,cov_xy,cov_xy,var_y), nrow = 2, byrow = F) # cov matrix
 
-x <- mvrnorm(1000,mu=rho, Sigma = c2)
-plot(x)
+# Create a bivariate normal Distribution
+nr_samples <- 10000
+biv_normal_dist <- mvrnorm(nr_samples,mu=rho, Sigma = c2)
+
+# If you want you can also plot the distribution
+# plot(biv_normal_dist)
+
+# a) calculate E[C] ----
+# C = 2*(X+Y)
+
+X <- biv_normal_dist[,1]
+Y <- biv_normal_dist[,2]
+
+expected_C <- mean(2*(X+Y))
+print("E[C] =")
+expected_C
+
+# b) calculate E[A] ----
+# A = X*Y
 
 
+expected_A <- mean(X*Y)
+print("E[A] =")
+expected_A
 
 
+# c) calculate P(C<29) + P(C>31)
 
+C <- 2*(X+Y)
+# no. Plates with C < 29
 
+too_small <- length(C[C<29])
+too_big <- length(C[C>31])
 
+probability_useless_plate <- (too_small + too_big)/nr_samples
+probability_useless_plate
 

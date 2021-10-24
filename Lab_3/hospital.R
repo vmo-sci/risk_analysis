64cf92c8d836764163fb918ea45b26910d62b748
Youheng Lü <youheng.lue@gmail.com>
2021-10-24 12:03:12 +0200

    Finished version from the group collection

--- Lab_3/hospital.R
+++ Lab_3/hospital.R
@@ -28,7 +28,9 @@ hospital_simulation <- function(nr_beds, t.end){
   n <- c()        # Vector of number of patients
   beds <- c()     # days the beds are occupied
   serviced <- 0   # customers serviced
+  all_serviced <- c()
   lost <- 0       # customers lost due to full queue
+  all_lost <- c()
   t.clock <- 0    # starting time
   
   
@@ -43,8 +45,6 @@ hospital_simulation <- function(nr_beds, t.end){
       # Kick out healthy patients
       beds <- beds[beds > 0]
     }
-    # Count the number of occupied beds
-    n <- c(n,length(beds))
     
     # New patients arrive ----
     new_p <- rpois(1, 14.8) # Amount of new patients
@@ -60,8 +60,18 @@ hospital_simulation <- function(nr_beds, t.end){
       serviced <- serviced + s
       # Only nr_beds people can stay
       # Will only kick out people who just moved in
-      beds <- beds[1:nr_beds]  
+
+      beds <- beds[1:nr_beds] 
+    } else {
+      s <- new_p
+      serviced <- serviced + s
     }
+    # Count the number of occupied beds
+    n <- c(n,length(beds))
+    # Lost patients per day
+    all_lost <- c(all_lost, lost)
+    all_serviced <- c(all_serviced, serviced)
+    
   }   
   
   # Print out the Results ----
@@ -82,9 +92,88 @@ hospital_simulation <- function(nr_beds, t.end){
   l <- length(n)
   plot(n[(l-100):l],type = 'l', main = 'Last 100 days', 
        xlab = 'day', ylab = 'patients')
-  return(n)
+  return(list(n, all_lost, all_serviced))
 }
 
 # hospital_40 will contain
-hospital_40 <- hospital_simulation(nr_beds = 40, t.end = 10^3)
+
+hospital_40 <- hospital_simulation(nr_beds = 40, t.end = 100+365)
+n <- unlist(hospital_40[1])
+all_lost <- unlist(hospital_40[2])
+all_serviced <- unlist(hospital_40[3])
+
+# occupancy rate: 
+n_observed <- n[101:465] # After 100 days we start observing hospital
+
+occupancy_rate <- n_observed/40
+
+# Solution to the first question:
+plot(occupancy_rate, type = 'l')
+
+# Solution to the second question:
+hospital_40 <- hospital_simulation(nr_beds = 40, t.end = 100+10000)
+lost <- unlist(hospital_40[2])
+plot(lost, type = 'l')
+serviced <- unlist(hospital_40[3])
+plot(serviced, type = 'l')
+
+lost[length(lost)]/(serviced[length(serviced)] + lost[length(lost)])
+
+# 12.3% patients are lost
+
+# Solution to the third question
+mean(n/40 <0.85) # We want this number to be 0.95 or greater
+
+end_result <- c()
+for(nr_bed in 1:100){
+  result <- hospital_simulation(nr_beds = nr_bed, t.end = 100)
+  bed_occ <- unlist(result[1])
+  occ_rate <- bed_occ/nr_bed
+  
+  lower_85 <- mean(occ_rate <= 0.85)
+  end_result <- c(end_result, lower_85)
+}
+
+end_result
+end_result[63]
+end_result[64]
+
+h_64 <- hospital_simulation(nr_beds = 64, t.end = 10^4)
+nr_patients <- unlist(h_64[1])/64
+mean(nr_patients <= 0.85)
+
+# Solution to problem 3:
+# We need 64 beds
+
+# Plotting----
+# function for creating file name with leading zeros
+# makes it easier to process them sequentially
+rename <- function(x){
+  if (x < 10) {
+    return(name <- paste('000',i,'plot.png',sep=''))
+  }
+  if (x < 100 && i >= 10) {
+    return(name <- paste('00',i,'plot.png', sep=''))
+  }
+  if (x >= 100) {
+    return(name <- paste('0', i,'plot.png', sep=''))
+  }
+}
+# Create pictures
+for(i in (1:365)){
+  name <- rename(i)
+  png(name) # save picture
+  plot(n, 
+       xlim = c(1, i),
+       type = 'l',
+       main = paste('Hospital simulation', i, 'days'),
+       xlab = 'Days',
+       ylab = 'Number of patients')
+  dev.off()
+}
+
+library(gifski)
+png_files <- list.files(".", pattern = "./*png$", full.names = TRUE)
+gifski(png_files, gif_file = "animation.gif", delay = 0.1)
+
 
